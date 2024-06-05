10befebec771689118b80f7950d9237f0f9cafc8
lucas-oristanio <lucas.faes@sptech.school>
2024-06-04 22:46:57 -0300

    Incrementando resposta 'Yes' automática no script SH

--- setup_script.sh
+++ setup_script.sh
@@ -2,13 +2,13 @@
 
 # Vai ser um setup diferente para cada integrante do grupo?
 echo "Desinstalando possíveis dependências e aplicações que possam causar conflito"
-sudo for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+# sudo for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
 # Adicionando chave GPG oficial Docker 
 echo "Adicionando chave GPG oficial Docker"
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
+sudo install -m 0755 -d /etc/apt/keyrings 
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
@@ -18,10 +18,10 @@ echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
     $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
     sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt-get update -y
 
 echo "Instalando pacotes Docker"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 
 # Instalar arquivo docker-compose.yml
 
